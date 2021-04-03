1ed82854ff315fbc95f70c9dab43027ced7655df
Gavin Zhao <74938940+GZGavinZhao@users.noreply.github.com>
2021-04-03 15:30:34 -0400

    Added sogoupinyin

---
+++ sogoupinyin/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+# Created For Solus Operating System
+
+from pisi.actionsapi import get, pisitools, shelltools
+
+NoStrip = ["/opt", "/usr", "/etc"]
+IgnoreAutodep = True
+
+# Suffix = "-1" Not applicable to this application
+
+def setup():
+    shelltools.system("pwd")
+    shelltools.system("ar xf sogoupinyin_%s_amd64.deb" % (get.srcVERSION()))
+    shelltools.system("tar xvf data.tar.xz")
+    shelltools.system("ln -s /opt/sogoupinyin/entries/icons/hicolor/128x128/fcitx-sogoupinyin.png /usr/share/pixmaps/sogoupinyin.png")
+
+def install():
+    pisitools.insinto("/", "opt")
+    pisitools.insinto("/", "usr")
+    pisitools.insinto("/", "etc")
