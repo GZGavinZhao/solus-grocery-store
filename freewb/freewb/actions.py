9a3e47db2554f61477403323fb95d71f56287d67
Gavin Zhao <74938940+GZGavinZhao@users.noreply.github.com>
2021-04-03 17:41:35 -0400

    Added freewb

---
+++ freewb/freewb/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+# Created For Solus Operating System
+
+from pisi.actionsapi import get, pisitools, shelltools
+
+NoStrip = ["/usr", "/etc"]
+IgnoreAutodep = True
+
+# Suffix = "-1" Not applicable to this application
+
+def setup():
+    shelltools.system("pwd")
+    shelltools.system("ar xf freewb_%s_amd64.deb" % (get.srcVERSION()))
+    shelltools.system("tar xvf data.tar.xz")
+    # shelltools.system("ln -s /opt/sogoupinyin/entries/icons/hicolor/128x128/fcitx-sogoupinyin.png /usr/share/pixmaps/sogoupinyin.png")
+
+def install():
+    pisitools.insinto("/", "usr")
+    pisitools.insinto("/", "etc")
