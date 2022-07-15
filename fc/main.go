f67f711d260595b53a4bf7cb91a6d14db0a9e0f7
Gavin Zhao <git@gzgz.dev>
2022-07-15 00:19:03 -0400

    fc: initial working version
    Signed-off-by: Gavin Zhao <git@gzgz.dev>

---
+++ fc/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"github.com/aliyun/aliyun-oss-go-sdk/oss"
+	"github.com/aliyun/fc-runtime-go-sdk/events"
+	"github.com/aliyun/fc-runtime-go-sdk/fc"
+	"github.com/aliyun/fc-runtime-go-sdk/fccontext"
+)
+
+const (
+	mntPoint   = "/mnt/grocery"
+	bucketName = "solus-grocery-store-oss"
+)
+
+func HandleRequest(ctx context.Context, event events.OssEvent) error {
+	fcctx, _ := fccontext.FromContext(ctx)
+	credentials := fcctx.Credentials
+	logger := fcctx.GetLogger()
+
+	logger.Debug("Uid:", os.Getuid())
+	logger.Debug("Gid:", os.Getgid())
+	logger.Debug("User:", fcctx.AccountId)
+
+	if _, err := os.Stat(mntPoint); err != nil {
+		if os.IsNotExist(err) {
+			logger.Error("NAS doesn't seem to be mounted!")
+		} else {
+			logger.Error(err)
+		}
+		return err
+	}
+
+	logger.Info("Try if we can write file to NAS...")
+	if err := os.WriteFile(filepath.Join(mntPoint, "test.txt"), []byte("Hello NAS from FC!"), 0666); err != nil {
+		logger.Error(err)
+	} else {
+		logger.Info("Successfully wrote file to NAS!")
+	}
+
+	logger.Info("Try if we can read file in NAS...")
+	if _, err := os.ReadFile(filepath.Join(mntPoint, "test.txt")); err != nil {
+		logger.Error(err)
+	} else {
+		logger.Info("Successfully read file in NAS!")
+	}
+
+	logger.Info("See if we can delete file in NAS...")
+	if err := os.Remove(filepath.Join(mntPoint, "test.txt")); err != nil {
+		logger.Error(err)
+	} else {
+		logger.Info("Successfully deleted file in NAS!")
+	}
+
+	logger.Info("Connecting to OSS using endpoint: " + "oss-" + fcctx.Region + "-internal.aliyuncs.com")
+	client, err := oss.New("oss-"+fcctx.Region+"-internal.aliyuncs.com", credentials.AccessKeyId, credentials.AccessKeySecret, oss.SecurityToken(credentials.SecurityToken))
+	if err != nil {
+		logger.Error(err)
+	} else {
+		logger.Info("Successfully connected to OSS!")
+	}
+
+	bucket, err := client.Bucket(bucketName)
+	if err != nil {
+		logger.Error(err)
+	}
+	if err := bucket.PutObject("fctest.txt", strings.NewReader("Hello OSS from FC!")); err != nil {
+		logger.Error(err)
+	} else {
+		logger.Info("Successfully wrote file to OSS!")
+	}
+
+	logger.Info("Listing top-level files in NAS...")
+	exec.Command("ls", mntPoint)
+
+	return nil
+}
+
+func main() {
+	fc.Start(HandleRequest)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module gitlab.com/solus-grocery-store/solus-grocery-store
 
 go 1.17
+
+require (
+	github.com/aliyun/aliyun-oss-go-sdk v2.2.4+incompatible // indirect
+	github.com/aliyun/fc-runtime-go-sdk v0.2.0 // indirect
+	github.com/getsolus/libeopkg v0.1.0 // indirect
+	golang.org/x/time v0.0.0-20220609170525-579cf78fd858 // indirect
+)
