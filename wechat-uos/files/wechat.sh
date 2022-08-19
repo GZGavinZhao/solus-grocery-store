d2912490d932fc1b499713eec0dff2c33572deee
Gavin Zhao <git@gzgz.dev>
2022-08-19 17:23:11 -0400

    wechat-uos: initial commit, v2.1.5
    Signed-off-by: Gavin Zhao <git@gzgz.dev>

---
+++ wechat-uos/files/wechat.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+wechat_pid=`pidof weixin`
+if test  $wechat_pid ;then
+    kill -9 $wechat_pid
+fi
+bwrap --dev-bind / / \
+    --bind /usr/share/wechat-uos/license/etc/os-release /etc/os-release \
+    --bind /usr/share/wechat-uos/license/etc/lsb-release /etc/lsb-release \
+    --bind /usr/lib/wechat-uos/ /usr/lib/license/ \
+    --bind /usr/share/wechat-uos/license/var/ /var/ \
+    /usr/share/wechat-uos/weixin "$@"
